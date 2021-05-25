6021bb980aa97f8ca7e84144f83035a89d3e2f87
Patrick Henn <patrickhenn@Air-von-Patrick.fritz.box>
2021-05-25 16:50:31 +0200

    some tests

--- script/main.js
+++ script/main.js
@@ -1,6 +1,9 @@
-let app = new Vue({
-    el:'#app',
-    data:{
-        product: 'Puppies'
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Puppies',
+        time: new Date(),
+        image: './public/img/dog-puppy.png',
+        github: 'https://github.com/',
     }
 }) 
